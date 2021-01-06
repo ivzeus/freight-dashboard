c38efbcb3faeb247fd883b557305a23d9b140f92
toanhn <toanhn@blockpass.org>
2021-01-06 17:22:19 +0700

    Updates

---
+++ precache-manifest.27f0e5b99dda21b241da037cb70beea7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da6889f524685e3aa366f32afeef362",
+    "url": "/freight-dashboard/index.html"
+  },
+  {
+    "revision": "74a5ab525a3b3b4c3c0d",
+    "url": "/freight-dashboard/static/css/2.5c985af2.chunk.css"
+  },
+  {
+    "revision": "74a5ab525a3b3b4c3c0d",
+    "url": "/freight-dashboard/static/js/2.e7dd5155.chunk.js"
+  },
+  {
+    "revision": "4f47f5d0075e3d9fe73a416105e80737",
+    "url": "/freight-dashboard/static/js/2.e7dd5155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c4d936e1258904c985",
+    "url": "/freight-dashboard/static/js/main.bffffd0c.chunk.js"
+  },
+  {
+    "revision": "b58055ecb85c863bcc60",
+    "url": "/freight-dashboard/static/js/runtime-main.2e37bb46.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/freight-dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/freight-dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/freight-dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/freight-dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/freight-dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/freight-dashboard/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/freight-dashboard/static/media/logo.a0185b04.svg"
+  }
+]);
